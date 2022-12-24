38fc5fac67dcbeffe10a1bd2536c63454fc75830
Yuriy Kravets <97830497+yuriykravets@users.noreply.github.com>
2022-12-24 14:00:36 +0200

    Made Animal superclass and set him open modifier

---
+++ src/main/kotlin/Animal.kt
@@ -0,0 +1,22 @@
+open class Animal {
+    open val image = ""
+    open val food = ""
+    open val habitat = ""
+    open var hunger = 10
+
+    open fun makeNoise() {
+        println("The Animal is making a noise")
+    }
+
+    open fun eat() {
+        println("The Animal is eating")
+    }
+
+    open fun roam() {
+        println("The Animal is roaming")
+    }
+
+    open fun sleep() {
+        println("The Animal is sleeping")
+    }
+}
